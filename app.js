3f423efe5f46c94f64ec8d2422bbfe642f682479
oscq12 <orianasofia86@gmail.com>
2022-07-18 13:26:43 +0000

    primera parte del random card

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-alert("Hola qye tal")
+window.onload=()=> {
+    let body = document.body
+    body.style.backgroundColor = "green"
+    body.style.height="100vh"
+    body.style.display="flex"
+    body.style.justifyContent="center"
+    body.style.alignItems="center"
+
+
+    let card = document.querySelector("#app")
+    card.style.backgroundColor="white"
+    card.style.border="1px solid black"
+    card.style.height="400px"
+    card.style.width="250px"
+    card.style.borderRadius="10px"
+    
+    
+
+}
