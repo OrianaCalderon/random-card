16c8def847b515824b462a623d834185c51fc9b4
oscq12 <orianasofia86@gmail.com>
2022-07-18 14:02:24 +0000

    algunos cambios

--- app.js
+++ app.js
@@ -13,7 +13,35 @@ window.onload=()=> {
     card.style.height="400px"
     card.style.width="250px"
     card.style.borderRadius="10px"
+    card.style.padding="15px"
+
+    let containerImg= document.createElement("div")
+    containerImg.textContent="imagen1"
+    card.appendChild(containerImg)
+    containerImg.style.position="absolute"
+    containerImg.style.positionTop="0px"
+    containerImg.style.positionLeft="0px"
+
+
+    let containerNumber= document.createElement("p")
+    containerNumber.textContent="numero"
+    card.appendChild(containerNumber)
+
+    let containerImgTwo= document.createElement("div")
+    containerImgTwo.textContent="imagen2"
+    card.appendChild(containerImgTwo)
+    containerImgTwo.style.position="absolute"
+    containerImgTwo.style.positionBottom="0px"
+    containerImgTwo.style.positionRight="0px"
+
+    let img = ["♦","♥" ,"♠" ,"♣"]
+    let numeros =["A", 1,2,3,4,5,6,7,8,9,10,"J","Q","K"]
+
+
     
+
+
     
+
 
 }
