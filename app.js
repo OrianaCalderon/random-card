a9ab4e20088ce58232c7a0c08e89afc29a5d3985
oscq12 <orianasofia86@gmail.com>
2022-07-18 18:58:51 +0000

    algunos cambios, faltan las funciones

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-window.onload=()=> {
+let cardSigns = ["♦","♥" ,"♠" ,"♣"]
+let cardNumbers =["A", "1","2","3","4","5","6","7","8","9","10","J","Q","K"]
+
+let randomCardSigns = () =>{
+    return Math.floor(Math.random() * cardSigns.length)}
+let randomCardNumbers = () => {
+    return Math.floor(Math.random() * cardNumbers.length)}
+
+
+
+
+    window.onload=()=> {
+
+
     let body = document.body
     body.style.backgroundColor = "green"
     body.style.height="100vh"
@@ -14,28 +27,51 @@ window.onload=()=> {
     card.style.width="250px"
     card.style.borderRadius="10px"
     card.style.padding="15px"
-
-    let containerImg= document.createElement("div")
-    containerImg.textContent="imagen1"
-    card.appendChild(containerImg)
-    containerImg.style.position="absolute"
-    containerImg.style.positionTop="0px"
-    containerImg.style.positionLeft="0px"
+    card.style.position="relative"
 
 
-    let containerNumber= document.createElement("p")
-    containerNumber.textContent="numero"
-    card.appendChild(containerNumber)
+
+    let containerImgOne= document.createElement("div")
+    containerImgOne.textContent="imagen1"
+    containerImgOne.style.position="absolute"
+    containerImgOne.style.top="1px"
+    containerImgOne.style.left="1px"
+    containerImgOne.style.width="45px"
+    containerImgOne.style.height="45px"
+    containerImgOne.style.border="1px solid black"
+    containerImgOne.style.margin="15px"
+
+
 
     let containerImgTwo= document.createElement("div")
     containerImgTwo.textContent="imagen2"
-    card.appendChild(containerImgTwo)
     containerImgTwo.style.position="absolute"
-    containerImgTwo.style.positionBottom="0px"
-    containerImgTwo.style.positionRight="0px"
+    containerImgTwo.style.border="1px solid black"
+    containerImgTwo.style.margin="10px"
+    containerImgTwo.style.right="5px"
+    containerImgTwo.style.top="350px"
+    containerImgTwo.style.height="50px"
+    containerImgTwo.style.width="50px"
+    
+    
+    let containerNumber= document.createElement("p")
+    containerNumber.textContent="numero"
+    containerNumber.style.height="50px"
+    containerNumber.style.width="50px"
+    containerNumber.style.border="1px solid black"
+    containerNumber.style.position="absolute"
+    containerNumber.style.top="175px"
+    containerNumber.style.height="50px"
+    containerNumber.style.right="110px"
 
-    let img = ["♦","♥" ,"♠" ,"♣"]
-    let numeros =["A", 1,2,3,4,5,6,7,8,9,10,"J","Q","K"]
+
+
+
+
+    card.appendChild(containerNumber)
+    card.appendChild(containerImgOne)
+    card.appendChild(containerImgTwo)
+    
 
 
     
